79c16654c855be0f20a96fcdc19e0744f5b5b684
백재현 <xkgun@naver.com>
2017-05-19 22:30:03 +0900

    코드 리펙토링

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -33,11 +33,11 @@ public class PrimeFactorsTest {
 
     private List<Integer> of(int n) {
         ArrayList<Integer> factors = new ArrayList<Integer>();
-        int divisor = 2;
-        for(;n > 1;divisor++){
+
+        for(int divisor = 2;n > 1;divisor++){
             for(;n % divisor ==0;n /= divisor) {
                 factors.add(divisor);
-            };
+            }
 
         }
         return factors;
